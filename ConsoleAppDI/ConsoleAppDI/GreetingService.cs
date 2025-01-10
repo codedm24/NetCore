6f4ef4f5e836e65baaf779ea6f7526a75fc60f70
codedm24 <mithun.adroit@gmail.com>
2025-01-10 19:03:39 +0530

    Dependency Injection App
    .NET Core Dependency Injection

---
+++ ConsoleAppDI/ConsoleAppDI/GreetingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNoDI
+{
+    //Without DI
+    //internal class GreetingService
+    //{
+    //    public string Greet(string name) => $"Hello, {name}";
+    //}
+
+    //using DI
+    internal class GreetingService : IGreetingService
+    {
+        public string Greet(string name) => $"Hello, {name}";
+    }
+
+}
