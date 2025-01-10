9672a1aa48b734b33aa9c5edfeb8a1e5acbc5c81
codedm24 <mithun.adroit@gmail.com>
2025-01-10 18:58:00 +0530

    Dependency Injection
    Dependency Injection in .NET Core

---
+++ ConsoleAppNoDI/ConsoleAppNoDI/HomeControllerNoDI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNoDI
+{
+    internal class HomeControllerNoDI
+    {
+        public string Hello(string name)
+        {
+            GreetingService greetingService = new GreetingService();
+            return greetingService.Greet(name);
+        }
+    }
+}
---
+++ ConsoleAppNoDI/ConsoleAppNoDI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleAppNoDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //without DI
+            var controllerNoDI = new HomeControllerNoDI();
+            string resultNoDI = controllerNoDI.Hello("Stephanie");
+            Console.WriteLine($"Output No DI:{resultNoDI}");
+
+            //using DI
+            IGreetingService greetingService = new GreetingService();
+            var controllerWithDI = new HomeController(greetingService);
+            string resultWithDI = controllerWithDI.Hello("Stephanie");
+            Console.WriteLine($"Output with DI: {resultWithDI}");
+
+            //using DIContainer
+            using (ServiceProvider container = RegisterServices())
+            { 
+                var controllerDIContainer = container.GetRequiredService<HomeController>();
+                string resultDIContainer = controllerDIContainer.Hello("Katharina");
+                Console.WriteLine($"Output with DI using ServiceProvider: {resultDIContainer}");
+            }
+        }
+
+        static ServiceProvider RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IGreetingService, GreetingService>();
+            services.AddTransient<HomeController>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
